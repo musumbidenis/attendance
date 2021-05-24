618ec4ed2d4a63bfac138b3aff68495c764678a2
Musumbi Denis <musumbidenis@gmail.com>
2021-05-24 13:35:32 +0300

    Updated project structure

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    School Attendance System
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- CSS Files -->
+  <link href="../assets/css/material-dashboard.css?v=2.1.2" rel="stylesheet" />
+</head>
+
+<body class="sidebar-mini">
+    <div class="wrapper ">
+        <div class="main-panel">
+            <!-- Sidebar -->
+            @include('layouts.sidebar')
+            <!-- Body content -->
+            @yield('content')
+
+        </div>
+    </div>
+    <!--   Core JS Files   -->
+    <script src="../assets/js/core/jquery.min.js"></script>
+    <script src="../assets/js/core/popper.min.js"></script>
+    <script src="../assets/js/core/bootstrap-material-design.min.js"></script>
+    <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+    <!-- Plugin for the momentJs  -->
+    <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="../assets/js/material-dashboard.js?v=2.1.2" type="text/javascript"></script>
+    <script>
+        $(document).ready(function() {
+        $().ready(function() {
+            $sidebar = $('.sidebar');
+
+            $sidebar_img_container = $sidebar.find('.sidebar-background');
+
+            $full_page = $('.full-page');
+
+            $sidebar_responsive = $('body > .navbar-collapse');
+
+            window_width = $(window).width();
+
+            fixed_plugin_open = $('.sidebar .sidebar-wrapper .nav li.active a p').html();
+
+            if (window_width > 767 && fixed_plugin_open == 'Dashboard') {
+            if ($('.fixed-plugin .dropdown').hasClass('show-dropdown')) {
+                $('.fixed-plugin .dropdown').addClass('open');
+            }
+
+            }
+
+            });
+        });
+        });
+    </script>
+</body>
+
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,72 @@
+<div class="sidebar" data-color="azure" data-background-color="white" data-image="../../assets/img/sidebar-1.jpg">
+    <div class="logo"><a href="" class="simple-text logo-mini">
+        SC
+      </a>
+      <a href="" class="simple-text logo-normal">
+        SYSTEM
+      </a></div>
+    <div class="sidebar-wrapper">
+
+      <!-- User profile details -->
+      <div class="user">
+        <div class="photo">
+          <img src="../assets/img/faces/avatar.jpg" />
+        </div>
+        <div class="user-info">
+          <a data-toggle="collapse" href="#collapseExample" class="username">
+            <span>
+              Tania Andrew
+              <b class="caret"></b>
+            </span>
+          </a>
+          <div class="collapse" id="collapseExample">
+            <ul class="nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span class="sidebar-mini"> MP </span>
+                  <span class="sidebar-normal"> My Profile </span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span class="sidebar-mini"> L </span>
+                  <span class="sidebar-normal"> Logout </span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <!-- Sidebar menu items -->
+      <ul class="nav">
+
+        <li class="nav-item active ">
+          <a class="nav-link" href="../examples/dashboard.html">
+            <i class="material-icons">dashboard</i>
+            <p> Dashboard </p>
+          </a>
+        </li>
+
+        <li class="nav-item ">
+          <a class="nav-link" data-toggle="collapse" href="#pagesExamples">
+            <i class="material-icons">image</i>
+            <p> Pages
+              <b class="caret"></b>
+            </p>
+          </a>
+          <div class="collapse" id="pagesExamples">
+            <ul class="nav">
+              <li class="nav-item ">
+                <a class="nav-link" href="../examples/pages/pricing.html">
+                  <span class="sidebar-mini"> P </span>
+                  <span class="sidebar-normal"> Pricing </span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </li>
+
+      </ul>
+    </div>
+  </div>
