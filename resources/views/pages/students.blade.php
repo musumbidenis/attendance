0474da951a2191d7af96e255a15f8232f28ed28f
Musumbi Denis <musumbidenis@gmail.com>
2021-06-03 20:13:34 +0300

    Added new student form

--- resources/views/pages/students.blade.php
+++ resources/views/pages/students.blade.php
@@ -8,7 +8,7 @@
                   <div class="students">
                     <h3 class="title text-center">Students</h3>
                     <br />
-                    <ul class="nav nav-pills nav-pills-warning nav-pills-icons justify-content-center" role="tablist">
+                    <ul class="nav nav-pills nav-pills-info nav-pills-icons justify-content-center" role="tablist">
                       <li class="nav-item">
                         <a class="nav-link" data-toggle="tab" href="#stats" role="tablist">
                           <i class="material-icons">pie_chart</i> Stats
@@ -26,14 +26,13 @@
                       </li>
                     </ul>
                     <div class="tab-content tab-space tab-subcategories">
-
                       <div class="tab-pane" id="stats">
                         <div class="content">
                           <div class="container-fluid">
                             <div class="row">
                               <div class="col-md-5">
                                 <div class="card card-chart">
-                                  <div class="card-header card-header-icon card-header-danger">
+                                  <div class="card-header card-header-icon card-header-primary">
                                     <div class="card-icon">
                                       <i class="material-icons">pie_chart</i>
                                     </div>
@@ -60,8 +59,6 @@
                           </div>
                         </div>
                       </div>
-
-
                       <div class="tab-pane active" id="search">
                         <div class="content">
                           <div class="container-fluid">
@@ -102,78 +99,6 @@
                                         </tfoot>
                                         <tbody>
                                           <tr>
-                                            <td>Prescott Bartlett</td>
-                                            <td>Technical Author</td>
-                                            <td>London</td>
-                                            <td>27</td>
-                                            <td>2011/05/07</td>
-                                            <td class="text-right">
-                                              <a href="#" class="btn btn-link btn-info btn-just-icon like"><i class="material-icons">favorite</i></a>
-                                              <a href="#" class="btn btn-link btn-warning btn-just-icon edit"><i class="material-icons">dvr</i></a>
-                                              <a href="#" class="btn btn-link btn-danger btn-just-icon remove"><i class="material-icons">close</i></a>
-                                            </td>
-                                          </tr>
-                                          <tr>
-                                            <td>Gavin Cortez</td>
-                                            <td>Team Leader</td>
-                                            <td>San Francisco</td>
-                                            <td>22</td>
-                                            <td>2008/10/26</td>
-                                            <td class="text-right">
-                                              <a href="#" class="btn btn-link btn-info btn-just-icon like"><i class="material-icons">favorite</i></a>
-                                              <a href="#" class="btn btn-link btn-warning btn-just-icon edit"><i class="material-icons">dvr</i></a>
-                                              <a href="#" class="btn btn-link btn-danger btn-just-icon remove"><i class="material-icons">close</i></a>
-                                            </td>
-                                          </tr>
-                                          <tr>
-                                            <td>Martena Mccray</td>
-                                            <td>Post-Sales support</td>
-                                            <td>Edinburgh</td>
-                                            <td>46</td>
-                                            <td>2011/03/09</td>
-                                            <td class="text-right">
-                                              <a href="#" class="btn btn-link btn-info btn-just-icon like"><i class="material-icons">favorite</i></a>
-                                              <a href="#" class="btn btn-link btn-warning btn-just-icon edit"><i class="material-icons">dvr</i></a>
-                                              <a href="#" class="btn btn-link btn-danger btn-just-icon remove"><i class="material-icons">close</i></a>
-                                            </td>
-                                          </tr>
-                                          <tr>
-                                            <td>Unity Butler</td>
-                                            <td>Marketing Designer</td>
-                                            <td>San Francisco</td>
-                                            <td>47</td>
-                                            <td>2009/12/09</td>
-                                            <td class="text-right">
-                                              <a href="#" class="btn btn-link btn-info btn-just-icon like"><i class="material-icons">favorite</i></a>
-                                              <a href="#" class="btn btn-link btn-warning btn-just-icon edit"><i class="material-icons">dvr</i></a>
-                                              <a href="#" class="btn btn-link btn-danger btn-just-icon remove"><i class="material-icons">close</i></a>
-                                            </td>
-                                          </tr>
-                                          <tr>
-                                            <td>Howard Hatfield</td>
-                                            <td>Office Manager</td>
-                                            <td>San Francisco</td>
-                                            <td>51</td>
-                                            <td>2008/12/16</td>
-                                            <td class="text-right">
-                                              <a href="#" class="btn btn-link btn-info btn-just-icon like"><i class="material-icons">favorite</i></a>
-                                              <a href="#" class="btn btn-link btn-warning btn-just-icon edit"><i class="material-icons">dvr</i></a>
-                                              <a href="#" class="btn btn-link btn-danger btn-just-icon remove"><i class="material-icons">close</i></a>
-                                            </td>
-                                          </tr>
-                                          <tr>
-                                            <td>Hope Fuentes</td>
-                                            <td>Secretary</td>
-                                            <td>San Francisco</td>
-                                            <td>41</td>
-                                            <td>2010/02/12</td>
-                                            <td class="text-right">
-                                              <a href="#" class="btn btn-link btn-info btn-just-icon like"><i class="material-icons">favorite</i></a>
-                                              <a href="#" class="btn btn-link btn-warning btn-just-icon edit"><i class="material-icons">dvr</i></a>
-                                              <a href="#" class="btn btn-link btn-danger btn-just-icon remove"><i class="material-icons">close</i></a>
-                                            </td>
-                                          </tr>
-                                          <tr>
                                             <td>Vivian Harrell</td>
                                             <td>Financial Controller</td>
                                             <td>San Francisco</td>
@@ -207,55 +132,54 @@
                           </div>
                         </div>
                       </div>
-
                       <div class="tab-pane" id="new">
                         <div class="content">
                           <div class="container-fluid">
                             <div class="row">
-                              <div class="col-md-6">
-                                <form id="RegisterValidation" action="" method="">
+                              <div class="col-md-6 ml-auto mr-auto">
+                                <form id="addStudent" action="" method="">
+
                                   <div class="card ">
-                                    <div class="card-header card-header-rose card-header-icon">
+                                    <div class="card-header card-header-primary card-header-icon">
                                       <div class="card-icon">
                                         <i class="material-icons">mail_outline</i>
                                       </div>
-                                      <h4 class="card-title">Register Form</h4>
+                                      <h4 class="card-title">New Student Form</h4>
                                     </div>
                                     <div class="card-body ">
                                       <div class="form-group">
-                                        <label for="exampleEmail" class="bmd-label-floating"> Email Address *</label>
-                                        <input type="email" class="form-control" id="exampleEmail" required="true">
+                                        <label class="bmd-label-floating"> Student Id *</label>
+                                        <input type="text" class="form-control" id="studentId" name="studentId" required="true">
                                       </div>
                                       <div class="form-group">
-                                        <label for="examplePassword" class="bmd-label-floating"> Password *</label>
-                                        <input type="password" class="form-control" id="examplePassword" required="true" name="password">
+                                        <label class="bmd-label-floating"> First Name *</label>
+                                        <input type="text" class="form-control" id="firstName" name="firstName" required="true">
                                       </div>
                                       <div class="form-group">
-                                        <label for="examplePassword1" class="bmd-label-floating"> Confirm Password *</label>
-                                        <input type="password" class="form-control" id="examplePassword1" required="true" equalTo="#examplePassword" name="password_confirmation">
+                                        <label class="bmd-label-floating"> Surname *</label>
+                                        <input type="text" class="form-control" id="surname" name="surname" required="true">
+                                      </div>
+                                      <div class="form-group">
+                                        <label class="bmd-label-floating"> Email Address *</label>
+                                        <input type="email" class="form-control" id="email" name="email" required="true">
+                                      </div>
+                                      <div class="form-group">
+                                        <label class="bmd-label-floating"> Phone Number *</label>
+                                        <input type="number" class="form-control" id="phone" name="phone" minLength="10" maxLength="10" required="true">
                                       </div>
                                       <div class="category form-category">* Required fields</div>
                                     </div>
                                     <div class="card-footer text-right">
-                                      <div class="form-check mr-auto">
-                                        <label class="form-check-label">
-                                          <input class="form-check-input" type="checkbox" value="" required> Subscribe to newsletter
-                                          <span class="form-check-sign">
-                                            <span class="check"></span>
-                                          </span>
-                                        </label>
-                                      </div>
                                       <button type="submit" class="btn btn-rose">Register</button>
                                     </div>
                                   </div>
+
                                 </form>
                               </div>
                             </div>
                           </div>
                         </div>
                       </div>
-
-
                     </div>
                   </div>
                 </div>
@@ -267,8 +191,10 @@
 <script src="../../assets/js/plugins/jquery.dataTables.min.js"></script>
 <script src="../../assets/js/plugins/jquery.validate.min.js"></script>
     <script>
-      function setFormValidation(id) {
-        $(id).validate({
+      $(document).ready(function() {
+        $('#addStudent').validate({
+          /* Add custom validation here -- rules,messages e.t.c */
+
           highlight: function(element) {
             $(element).closest('.form-group').removeClass('has-success').addClass('has-danger');
             $(element).closest('.form-check').removeClass('has-success').addClass('has-danger');
@@ -281,10 +207,6 @@
             $(element).closest('.form-group').append(error);
           },
         });
-      }
-
-      $(document).ready(function() {
-        setFormValidation('#RegisterValidation');
       });
     </script>
     <script>
