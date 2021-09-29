ec9542a24e3cbb3ba9ec5e532b774f50c22c42fa
musumbidenis <musumbidenis@gmail.com>
2021-09-29 15:00:18 +0300

    Edited Tutor USSD Menu

--- app/Http/Controllers/TutorsController.php
+++ app/Http/Controllers/TutorsController.php
@@ -74,140 +74,145 @@ class TutorsController extends Controller
         if ($text == "") {
 
             // first response when a user dials our ussd code
-            $response  = "CON Welcome. Select your category \n";
+            $response  = "CON Welcome. Reply with \n";
             $response .= "1. Tutor \n";
             $response .= "2. Student";
+
+            echo $response;
 
         }else{
 
             switch ($text) {
                 case 1:
                     //Tutor Menu
-                    $this->tutorMenu($ussd_string_exploded);
-                    // $response = "CON Please enter your Tutor ID";
+                    $this->tutorMenu($ussd_string_exploded, $phoneNumber);
                     break;
                 case 2:
                     //Student Menu
+                    $this->studentMenu($ussd_string_exploded);
                     break;
                 default:
                     //Invalid choice
-                    $response = "END Invalid choice. Please try again.";
+                    echo "END Invalid choice. Please try again.";
                     break;
-            }
-
-            
+            }   
 
         }
-        // elseif ($level == 2) {
 
-        //     //Verify the Tutor ID
-        //     $tutorId = $ussd_string_exploded[1];
-        //     $credentials = Tutor::where('tutorId', $tutorId)->count();
-
-        //     //Tutor record does not exist
-        //     if ($credentials == 0) {
-
-        //         $response = "END Tutor ID record does not exist. Please contact the system admin for assistance.";
-
-        //     //Tutor record exists
-        //     }else{
-
-        //         //Verify Phone Number against Tutor ID
-        //         $credentials = Tutor::where('tutorId', $tutorId)->where('phone', $phoneNumber)->count();
-
-        //         if ($credentials == 0) {
-
-        //             $response = "END Phone Number is not registered to $tutorId. Contact the system admin for assistance.";
-
-        //         }else{
-
-        //             //Use the Tutor ID and get units they teach
-        //             $units = Unit::where('tutorId', $tutorId)->get();
-
-        //             $i = 1;
-
-        //             $response  = "CON Select unit to create a lesson.\n";
-        //             foreach ($units as $unit) {
-        //                 $response .= "$i. $unit->unitCode \n";
-        //                 $i++;
-        //             }
-
-        //         }
-        //     }
-        
-        // }elseif ($level == 3) {
-
-        //     //Get the Unit Code selected
-        //     $tutorId = $ussd_string_exploded[1];
-        //     $units = Unit::where('tutorId', $tutorId)->get();
-        //     $unitCode = $units[$ussd_string_exploded[2]-1]->unitCode;
-
-        //     $response = "CON Set the starting time for $unitCode in the following formart, e.g 08:00";
-
-        // }elseif($level == 4){
-
-        //     $tutorId = $ussd_string_exploded[1];
-        //     $units = Unit::where('tutorId', $tutorId)->get();
-        //     $unitCode = $units[$ussd_string_exploded[2]-1]->unitCode;
-        //     $lessonStart = $ussd_string_exploded[3];
-
-        //     $response = "CON You're setting $unitCode lesson to start at $lessonStart. \n";
-        //     $response .= "1. Save \n";
-        //     $response .= "2. Cancel";
-
-        // }elseif($level == 5) {
-
-        //     if($ussd_string_exploded[4] == '1'){
-                
-        //         //User chose saving lesson data
-        //         $date = date('Y-m-d');
-        //         $time = $ussd_string_exploded[3];
-        //         $lessonStart = $date.' '. $time;
-        //         $lessonStop = date('Y-m-d H:i', strtotime('+2 hours', strtotime($lessonStart)));
-        //         $stop = now();
-                
-        //         //Get the selected Unit Code using Tutors ID and response
-        //         $tutorId = $ussd_string_exploded[1];
-        //         $units = Unit::where('tutorId', $tutorId)->get();
-        //         $unitCode = $units[$ussd_string_exploded[2]-1]->unitCode;
-
-        //         //Store lesson record to DB
-        //         $lesson = new Lesson();
-        //         $lesson->lessonStart = $lessonStart;
-        //         $lesson->lessonStop = $lessonStop;
-        //         $lesson->tutorId = $tutorId;
-        //         $lesson->unitCode = $unitCode;
-
-        //         $lesson->save();
-
-        //         $response = "END Lesson for $unitCode has been created successfully";
-
-        //     }else{
-
-        //         $response = "END You canceled lesson creation process";
-
-        //     }
-        // }elseif ($text == "2") {
-
-        //     //If user selected 2, send them to the student menu
-        //     $response = "CON Please enter your Admission Number";
-
-        // }elseif ($level == 2) {
-
-        //     $response = "CON $ussd_string_exploded[1]";
-            
-        // }
-        // send your response back to the API
-        header('Content-type: text/plain');
-        echo $response;
+        // // send your response back to the API
+        // header('Content-type: text/plain');
     }
 
-    Public function tutorMenu($ussd_string_exploded){
+    /** Tutor USSD Menu */
+    Public function tutorMenu($ussd_string_exploded, $phoneNumber){
         // Get ussd menu level number from the gateway
         $level = count($ussd_string_exploded);
 
         if ($level == 1) {
-           echo "CON Please enter your Tutor ID";
+           echo "CON Enter your Tutor ID";
+        }elseif ($level == 2) {
+
+            //Verify the Tutor ID
+            $tutorId = $ussd_string_exploded[1];
+            $credentials = Tutor::where('tutorId', $tutorId)->count();
+
+            //Tutor record does not exist
+            if ($credentials == 0) {
+
+                echo "END Tutor ID record does not exist. Contact the system admin for assistance.";
+
+            //Tutor record exists
+            }else{
+
+                //Verify Phone Number against Tutor ID
+                $verification = Tutor::where('tutorId', $tutorId)->where('phone', $phoneNumber)->count();
+
+                if ($verification == 0) {
+
+                    echo "END Phone Number is not registered to $tutorId. Contact the system admin for assistance.";
+
+                }else{
+
+                    //Use the Tutor ID and get units they teach
+                    $units = Unit::where('tutorId', $tutorId)->get();
+
+                    $i = 1;
+
+                    $response  = "CON Select unit to create a lesson.\n";
+                    foreach ($units as $unit) {
+                        $response .= "$i. $unit->unitCode \n";
+                        $i++;
+                    }
+
+                    echo $response;
+
+                }
+            }
+        
+        }elseif ($level == 3) {
+
+            //Get the Unit Code selected
+            $tutorId = $ussd_string_exploded[1];
+            $units = Unit::where('tutorId', $tutorId)->get();
+            $unitCode = $units[$ussd_string_exploded[2]-1]->unitCode;
+
+            echo "CON Set the starting time for $unitCode in the following formart, e.g 08:00";
+
+        }elseif($level == 4){
+
+            $tutorId = $ussd_string_exploded[1];
+            $units = Unit::where('tutorId', $tutorId)->get();
+            $unitCode = $units[$ussd_string_exploded[2]-1]->unitCode;
+            $lessonStart = $ussd_string_exploded[3];
+
+            $response = "CON You're setting $unitCode lesson to start at $lessonStart. \n";
+            $response .= "1. Confirm \n";
+            $response .= "2. Cancel";
+
+            echo $response;
+
+        }elseif($level == 5) {
+
+            if($ussd_string_exploded[4] == '1'){
+                
+                //User chose saving lesson data
+                $date = date('Y-m-d');
+                $time = $ussd_string_exploded[3];
+                $lessonStart = $date.' '. $time;
+                $lessonStop = date('Y-m-d H:i', strtotime('+2 hours', strtotime($lessonStart)));
+                $stop = now();
+                
+                //Get the selected Unit Code using Tutors ID and response
+                $tutorId = $ussd_string_exploded[1];
+                $units = Unit::where('tutorId', $tutorId)->get();
+                $unitCode = $units[$ussd_string_exploded[2]-1]->unitCode;
+
+                //Store lesson record to DB
+                $lesson = new Lesson();
+                $lesson->lessonStart = $lessonStart;
+                $lesson->lessonStop = $lessonStop;
+                $lesson->tutorId = $tutorId;
+                $lesson->unitCode = $unitCode;
+
+                $lesson->save();
+
+                echo "END Lesson for $unitCode has been created successfully";
+
+            }else{
+
+                echo "END You canceled lesson creation process";
+
+            }
+        }
+    }
+
+    /** Student USSD Menu */
+    Public function studentMenu($ussd_string_exploded, $phoneNumber){
+        // Get ussd menu level number from the gateway
+        $level = count($ussd_string_exploded);
+
+        if ($level == 1) {
+            echo "Enter your admission number in the format";
         }
     }
 }
