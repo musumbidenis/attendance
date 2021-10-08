6537f57a44827820accf146e40a805688458ab03
musumbidenis <musumbidenis@gmail.com>
2021-10-09 01:20:14 +0300

    Created USSD Controller, Changed USSD API

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::post('ussd', 'StudentsController@onlineUssdMenu');
-Route::post('createLesson', 'TutorsController@createLesson');
+Route::post('ussd', 'USSDController@ussdMenu');
